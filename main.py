0763ef28372ebcee6d8c1dca2c5228b1a03a817a
a50tt <alexsarraolmo@gmail.com>
2025-04-11 01:27:28 +0200

    capped fps to 60

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    #Screen (filled black)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.fill(color=(0, 0, 0))
+    #FPS limiter
+    clock = pygame.time.Clock()
+    dt = 0
     # Game loop
     while True:
         for event in pygame.event.get():
+            print(event)
             if event.type == pygame.QUIT:
                 return
-            else:
-                print(event)
+        #Each frame, tick and save the milliseconds passed in 'dt'
+        dt = clock.tick(60)
+        print(f"dt --> {dt}")
 
 
 if __name__ == "__main__":

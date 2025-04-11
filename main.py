ed1e3e53dc3cb7de91311ba0edde687f8827ad44
a50tt <alexsarraolmo@gmail.com>
2025-04-11 02:22:47 +0200

    containers and movement added to player

--- main.py
+++ main.py
@@ -3,29 +3,45 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     #Screen (filled black)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill(color=(0, 0, 0))
+
     #FPS limiter
     clock = pygame.time.Clock()
     dt = 0
-    # Game loop
+
+    #Groups
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    #Instantiate PLAYER
+    Player.containers = (updatables, drawables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # -- GAME LOOP --
+
+    # Event handler
     while True:
         for event in pygame.event.get():
             print(event)
             if event.type == pygame.QUIT:
                 return
-        #Each frame, tick and save the milliseconds passed in 'dt'
-        dt = clock.tick(60)
-        print(f"dt --> {dt}")
+        screen.fill("black")
 
+        # Redraw player
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+
+        #Refreshes the 'Screen' object
+        pygame.display.flip()
+        # Each frame, tick and save the milliseconds passed in 'dt'
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
